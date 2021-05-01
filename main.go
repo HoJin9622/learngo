ea6bb5ba2a7fac4a83ba5a06c5e5e3c7207b40e1
이호진 <kiss0104040@gmail.com>
2021-05-01 16:11:24 +0900

    Functions part One

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
-	const name string = "hojin"
-	fmt.Println(name)
+	fmt.Println(multiply(2, 2))
+	totalLength, _ := lenAndUpper("hojin")
+	fmt.Println(totalLength)
 
-	var nickname string = "nico"
-	nickname = "coco"
-	fmt.Println(nickname)
-
-	score := 20 // 축약형은 오로지 func 안에서만 사용 가능
-	score = 40
-	fmt.Println(score)
+	repeatMe("hojin", "sanghyun", "go", "java")
 }
