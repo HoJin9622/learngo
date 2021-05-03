87458db0c56419d2930122ded02322a64f2d6b44
이호진 <kiss0104040@gmail.com>
2021-05-03 23:18:00 +0900

    Functions part Two

--- main.go
+++ main.go
@@ -5,22 +5,15 @@ import (
 	"strings"
 )
 
-func multiply(a, b int) int {
-	return a * b
-}
-
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
+// naked return
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	fmt.Println(multiply(2, 2))
-	totalLength, _ := lenAndUpper("hojin")
-	fmt.Println(totalLength)
-
-	repeatMe("hojin", "sanghyun", "go", "java")
+	totalLength, up := lenAndUpper("hojin")
+	fmt.Println(totalLength, up)
 }
