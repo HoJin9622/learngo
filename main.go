869a26eb7817400168afc20e076b9ddd7525aab8
이호진 <kiss0104040@gmail.com>
2022-01-08 23:52:42 +0900

    for, range ...args

--- main.go
+++ main.go
@@ -5,15 +5,42 @@ import (
 	"strings"
 )
 
+// 상수 - 변경 불가능
+// const name string = "nico"
+// 변수 - 변경 가능
+// var name string = "nico"
+// 축약형
+// name := "nico" - 한줄 위와 같은 뜻, Go가 타입을 자동으로 찾아준다. 그리고 func 안에서만 사용 가능하다.
+
+// 여러개의 변수를 받을 수 있는 함수
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
 // naked return
 func lenAndUpper(name string) (length int, uppercase string) {
+	// defer는 function이 끝난 후 작동한다
 	defer fmt.Println("I'm done")
 	length = len(name)
 	uppercase = strings.ToUpper(name)
 	return
 }
 
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	// for i:= 0; i < len(numbers); i++ {
+	// 	fmt.Println(numbers[i])
+	// }
+	return total
+}
+
 func main() {
-	totalLength, up := lenAndUpper("hojin")
-	fmt.Println(totalLength, up)
+	// repeatMe("hi", "bye", "hello", "yo")
+	// totalLength, up := lenAndUpper("hojin")
+	// fmt.Println(totalLength, up)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
