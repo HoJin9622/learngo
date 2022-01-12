efe540308dcdc8d92e5dad0f051c40e830a93564
이호진 <kiss0104040@gmail.com>
2022-01-13 00:41:54 +0900

    Update Delete

--- main.go
+++ main.go
@@ -121,16 +121,33 @@ func main() {
 	// } else {
 	// 	fmt.Println(definition)
 	// }
-	word := "hello"
-	definition := "Greeting"
-	err := dictionary.Add(word, definition)
+	// word := "hello"
+	// definition := "Greeting"
+	// err := dictionary.Add(word, definition)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// hello, _ := dictionary.Search(word)
+	// fmt.Println("found", word, "definition: ", hello)
+	// err2 := dictionary.Add(word, definition)
+	// if err2 != nil {
+	// 	fmt.Println(err2)
+	// }
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	// err := dictionary.Update(":asd", "Second")
+	// if err != nil {
+		// fmt.Println(err)
+	// }
+	// word, _ := dictionary.Search(baseWord)
+	// fmt.Println(word)
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(word)
 	}
-	hello, _ := dictionary.Search(word)
-	fmt.Println("found", word, "definition: ", hello)
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2)
-	}
+
 }
