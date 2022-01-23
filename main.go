f8325854b279fa21dd4bee8104afa733237b987b
이호진 <kiss0104040@gmail.com>
2022-01-23 16:07:15 +0900

    Slow URLChecker

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 var errReqeustFailed = errors.New("Request Failed")
 
 func main() {
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -21,7 +22,15 @@ func main() {
 		"https://academy.nomadcoders.co/",
 	}
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAIL"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
@@ -29,6 +38,7 @@ func hitURL(url string) error {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
 		return errReqeustFailed
 	}
 	return nil
