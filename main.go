d3828966eb19dec91d8f950fbb93f5c05cab27b7
이호진 <kiss0104040@gmail.com>
2022-01-23 16:17:25 +0900

    Goroutines

--- main.go
+++ main.go
@@ -4,34 +4,38 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 var errReqeustFailed = errors.New("Request Failed")
 
 func main() {
-	var results = make(map[string]string)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	}
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAIL"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	// var results = make(map[string]string)
+	// urls := []string{
+	// 	"https://www.airbnb.com/",
+	// 	"https://www.google.com/",
+	// 	"https://www.amazon.com/",
+	// 	"https://www.reddit.com/",
+	// 	"https://www.google.com/",
+	// 	"https://soundcloud.com/",
+	// 	"https://www.facebook.com/",
+	// 	"https://www.instagram.com/",
+	// 	"https://academy.nomadcoders.co/",
+	// }
+	// for _, url := range urls {
+	// 	result := "OK"
+	// 	err := hitURL(url)
+	// 	if err != nil {
+	// 		result = "FAIL"
+	// 	}
+	// 	results[url] = result
+	// }
+	// for url, result := range results {
+	// 	fmt.Println(url, result)
+	// }
+	go sexyCount("nico")
+	go sexyCount("flynn")
+	time.Sleep(time.Second * 5)
 }
 
 func hitURL(url string) error {
@@ -42,4 +46,11 @@ func hitURL(url string) error {
 		return errReqeustFailed
 	}
 	return nil
+}
+
+func sexyCount(person string) {
+	for i:= 0;i<10;i++{
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
+	}
 }
