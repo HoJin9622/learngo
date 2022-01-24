42a59a865c3cadf066894215e8abc2288d82b17e
이호진 <kiss0104040@gmail.com>
2022-01-24 22:37:39 +0900

    File Download

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"os"
 	"strings"
 
 	"github.com/HoJin9622/learngo/scrapper"
 	"github.com/labstack/echo"
 )
 
+const filaName string = "jobs.csv"
+
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 
 func handleScrape(c echo.Context) error {
+	defer os.Remove(filaName)
 	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
-	return nil
+	scrapper.Scrape(term)
+	return c.Attachment(filaName, filaName)
 }
 
 func main() {
